b87ae82aa4d471c7f3ea8ce06cdec0dee5e8a1fd
ASt4Github <adam.steiner@student.gyarab.cz>
2024-11-03 13:03:22 +0100

    Otaceni Vlajeckovani Hromadne odhalovani

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+var pulbPole = [];
+var odkryto = [];
+var vlajecka = [];
+var sirka;
+var vyska;
+
+
 function strankoPrestup(param) {
     window.location.href = 'Game.html?myParam=' + encodeURIComponent(param);
 }
@@ -25,14 +32,20 @@ function nacti(){
         if(myParam == "Lehka"){
             console.log("lehka");
             stav(9, 9, 10);
+            sirka = 8;
+            vyska = 8;
         }
         if(myParam == "Stredni"){
             console.log("stredni");
             stav(16, 16, 40);
+            sirka = 15;
+            vyska = 15;
         }
         if(myParam == "Tezka"){
             console.log("tezka");
             stav(16, 30, 99);
+            sirka = 29;
+            vyska = 15;
         }
         
     }
@@ -53,7 +66,7 @@ function stav(radky, sloupce, pocet){
         if((pole[radRad][sloRad] != 9) && (pole[radRad][sloRad] != null)){
             pole[radRad][sloRad] += 9;
             if((radRad+1 >= 0) && (sloRad >= 0) && (radRad+1 < radky) && (sloRad < sloupce)) pole[radRad+1][sloRad]++;             
-            if((radRad-1 > 0) && (sloRad > 0) && (radRad-1 < radky) && (sloRad < sloupce)) pole[radRad-1][sloRad]++;
+            if((radRad-1 >= 0) && (sloRad >= 0) && (radRad-1 < radky) && (sloRad < sloupce)) pole[radRad-1][sloRad]++;
             if((radRad+1 >= 0) && (sloRad+1 >= 0) && (radRad+1 < radky) && (sloRad < sloupce+1)) pole[radRad+1][sloRad+1]++;
             if((radRad-1 >= 0) && (sloRad+1 >= 0) && (radRad-1 < radky) && (sloRad+1 < sloupce)) pole[radRad-1][sloRad+1]++;
             if((radRad+1 >= 0) && (sloRad-1 >= 0) && (radRad+1 < radky) && (sloRad-1 < sloupce)) pole[radRad+1][sloRad-1]++;
@@ -64,42 +77,10 @@ function stav(radky, sloupce, pocet){
         }
     }
 
+    pulbPole = pole;
     vystav(radky, sloupce, pole);
 
 }
-
-/*
-function vystav(radky, sloupce, pole) {
-    //element tabulka
-    const table = document.createElement("table");
-    table.style.borderCollapse = "collapse";
-
-    for (let i = 0; i < radky; i++) {
-        const row = document.createElement("tr"); // Vytvoří nový řádek
-        for (let j = 0; j < sloupce; j++) {
-            const cell = document.createElement("td"); // Vytvoří buňku
-            cell.style.border = "1px solid black";
-            cell.style.padding = "10px";
-            //if(pole[i][j]==1){
-                //add https://www.w3schools.com/howto/howto_css_shake_image.asp
-                //createFlipBox("img_paris.jpg", "Paris", "What an amazing city");
-                //cell.appendChild(flipBox)
-            //}
-            cell.textContent = `Řádek ${i + 1}, Sloupec ${j + 1}`;
-            //cell.textContent = `Řádek ${i + 1}, Sloupec ${j + 1}`; // Nastaví text buňky
-            row.appendChild(cell); // Přidá buňku do řádku
-        }
-        table.appendChild(row); // Přidá řádek do tabulky
-    }
-
-    // Přidá tabulku do kontejneru na stránce
-    document.getElementById("ErrInfo").appendChild(table);
-    //document.getElementById("tableContainer").appendChild(table);
-}
-*/
-
-//data info
-//data set
 
 function vystav(radky, sloupce, pole) {
     //element tabulka
@@ -113,54 +94,28 @@ function vystav(radky, sloupce, pole) {
             const img = document.createElement("img");
             cell.style.border = "0px solid black";
             cell.style.padding = "2px";
+
             console.log(pole[i][j])
 
             if(pole[i][j] == 0){
                 // Vytvoří obrázek a nastaví jeho vlastnosti
-                console.log("jsem v metodě");
-                img.src = "img/ZBomb.png";
-                img.alt = `Nulla ${i + 1}, Sloupec ${j + 1}`;
-                img.style.width = "50px";
-                img.style.height = "50px";
+                cell.appendChild(flipoTvor("ZBomb", i, j));
+                row.appendChild(cell);
             } else
 
-            if((pole[i][j] > 0) || (pole[i][j] < 9)){
+            if((pole[i][j] > 0) && (pole[i][j] < 9)){
                 // Vytvoří obrázek a nastaví jeho vlastnosti
                 let index = pole[i][j];
-                img.src = "img/" + index + "Bomb.png";
-                img.alt = `Řádek ${i + 1}, Sloupec ${j + 1}`;
-                img.style.width = "50px";
-                img.style.height = "50px";
+                cell.appendChild(flipoTvor(index + "Bomb", i, j));
+                row.appendChild(cell);
             }
 
             if(pole[i][j] >= 9){
                 // Vytvoří obrázek a nastaví jeho vlastnosti
-                let index = pole[i][j];
-                img.src = "img/Bomb.png";
-                img.alt = `Řádek ${i + 1}, Sloupec ${j + 1}`;
-                img.style.width = "50px";
-                img.style.height = "50px";
+                cell.appendChild(flipoTvor("Bomb", i, j));
+                row.appendChild(cell);
             }
-
-            cell.appendChild(img);
-
-            row.appendChild(cell);
-
-            /*
-            // Vytvoří obrázek a nastaví jeho vlastnosti
-            const img = document.createElement("img");
-            img.src = "img/Unknowpoint.png"; // Nahraďte URL_OBRÁZKU skutečnou URL
-            img.alt = `Řádek ${i + 1}, Sloupec ${j + 1}`;
-            img.style.width = "50px"; // Nastaví šířku obrázku
-            img.style.height = "50px"; // Nastaví výšku obrázku
-            */
-           
-            // Přidá obrázek do buňky
-            /*
-            cell.appendChild(img);
-
-            row.appendChild(cell); // Přidá buňku do řádku
-            */
+            //https://stackoverflow.com/questions/59927377/how-to-flip-image-on-click-in-javascript
         }
         table.appendChild(row); // Přidá řádek do tabulky
     }
@@ -169,8 +124,171 @@ function vystav(radky, sloupce, pole) {
     document.getElementById("ErrInfo").appendChild(table);
 }
 
-// Zavolá funkci pro vytvoření tabulky 4x3
+function flipoTvor(druh, i, j){
+    const flipCard = document.createElement('div');
+    flipCard.className = 'flip-card';
 
+    // Vytvoření vnitřního kontejneru
+    const flipCardInner = document.createElement('div');
+    flipCardInner.className = 'flip-card-inner';
+
+    // Přední strana karty
+    const flipCardFront = document.createElement('div');
+    flipCardFront.className = 'flip-card-front';
+
+    // Přidání event listeneru pro kliknutí
+    flipCard.addEventListener('click', function handleClick(event) {
+        flipCardInner.classList.add('flipped');
+        // Odebrání event listeneru po prvním kliknutí
+        flipCard.removeEventListener('click', handleClick);
+    });
+    
+
+    const img = document.createElement('img');
+    img.src = 'img/Unknowpoint.png';
+    img.alt = 'Neznamo';
+    img.id = i + ' + ' + j;
+    img.style.width = '50px';
+    img.style.height = '50px';
+
+    flipCardFront.appendChild(img);
+
+    // Zadní strana karty
+    const flipCardBack = document.createElement('div');
+    flipCardFront.className = 'flip-card-front';
+
+    const imgH = document.createElement('img');
+    imgH.src = 'img/'+ druh +'.png';
+    imgH.alt = druh;
+    imgH.id = i + ' + ' + j;
+    imgH.style.width = '50px';
+    imgH.style.height = '50px';
+
+    flipCardBack.appendChild(imgH);
+    // Sestavení karty
+    flipCardInner.appendChild(flipCardFront);
+    flipCardInner.appendChild(flipCardBack);
+    flipCard.appendChild(flipCardInner);
+
+    // Přidání flip karty do těla dokumentu
+    return flipCard;
+}
+
+document.addEventListener("contextmenu", function(event) {
+    if (event.target.tagName === 'IMG') {
+        // Získáme ID obrázku
+        const pozice = event.target.id;
+        if(!vlajecka.includes(pozice)){
+            vlajecka.push(pozice);
+        }
+        
+        console.log(vlajecka);
+
+        console.log(pozice);
+        const delidlo = pozice.split(" ");
+        let i = delidlo[0];
+        let j = delidlo[2];
+
+        // Změníme zdroj obrázku na nový obrázek
+        document.getElementById(i + ' + ' + j).src = "img/Marker.png";
+        
+        // Zamezíme výchozímu kontextovému menu
+        event.preventDefault();
+    }
+});
+
+document.addEventListener("contextmenu", function(event) {
+    if (event.ctrlKey && event.target.tagName === 'IMG') {
+        // Získáme ID obrázku
+        const pozice = event.target.id;
+        vlajecka = vlajecka.filter(function(item) {
+            return item !== pozice
+        })
+
+        console.log(vlajecka);
+
+        console.log(pozice);
+        const delidlo = pozice.split(" ");
+        let i = delidlo[0];
+        let j = delidlo[2];
+
+        // Přepínání obrázku mezi původním a novým
+        document.getElementById(i + ' + ' + j).src = "img/Unknowpoint.png";
+        
+        // Zamezíme výchozímu kontextovému menu
+        event.preventDefault();
+    }
+});
+
+function changeImage() {
+    // Zde definujte cestu k novému obrázku
+    return "img/Marker.png"; // Změňte na skutečnou cestu k obrázku
+}
+
+document.addEventListener('click', function(event) {
+    // Zkontrolujeme, zda bylo kliknuto na obrázek
+    if (event.target.tagName === 'IMG') {
+        // Získáme ID obrázku
+        const pozice = event.target.id;
+        odkryto.push(pozice);
+
+        console.log(pozice);
+        const delidlo = pozice.split(" ");
+        let i = delidlo[0];
+        let j = delidlo[2];
+
+        if(pulbPole[i][j] < 1){
+            console.log("mám " + pulbPole[i][j] + " a volnost!");
+            odkryvac(i, j);
+
+        }
+
+        if(pulbPole[i][j] > 8){
+            console.log("mám " + pulbPole[i][j] + " a Bombu!");
+        }
+        
+    }
+});
+
+function odkryvac(i, j){
+    let lokal = pulbPole[i][j];
+
+    let forward = i - 1;
+    let down = i - 0 + 1;
+    let right = j - 0 + 1;
+    let left = j - 0 - 1;
+
+    var nahoru = -1;
+    var dolu = -1;
+    var leva = -1;
+    var prava = -1;
+
+    console.log(i + " + " + j + "; " + forward + "-" + j + " + " + down + "-" + j + " + " + i + "-" + right + " + " + i + "-" + left);
+    if((forward != null)&&(forward > -1)&&(forward < vyska)) {nahoru = pulbPole[forward][j];} //neumyslny hack
+    if((down != null)&&(down > -1)&&(down < vyska)) {dolu = pulbPole[down][j];}
+    if((left != null)&&(left > -1)&&(left < sirka)) {leva = pulbPole[i][left];} //neumyslny hack
+    if((right != null)&&(right > -1)&&(right < sirka)) {prava = pulbPole[i][right];} //neumyslny hack
+
+    if((nahoru == 0)||((lokal == 0)&&(nahoru != -1)&&(nahoru < vyska))){
+        console.log(nahoru);
+        document.getElementById(forward + ' + ' + j).click();
+    }
+
+    if((dolu == 0)||((lokal == 0)&&(dolu != -1)&&(dolu < vyska))){
+        console.log(nahoru);
+        document.getElementById(down + ' + ' + j).click();
+    }
+
+    if((leva == 0)||((lokal == 0)&&(leva != -1)&&(leva < sirka))){
+        console.log(leva);
+        document.getElementById(i + ' + ' + left).click();
+    }
+
+    if((prava == 0)||((lokal == 0)&&(prava != -1)&&(prava < sirka))){
+        console.log(prava);
+        document.getElementById(i + ' + ' + right).click();
+    }
+}
 
 function poleVytvarec(radky, sloupce, naplnene = 0) {
     const array = new Array(radky);
